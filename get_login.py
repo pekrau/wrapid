6370a4ec0463a7cfc95a00b80023735a941fe262
Per Kraulis <per.kraulis@scilifelab.se>
2012-01-12 17:27:36 +0100

    Added get_login.py.

---
+++ get_login.py
@@ -0,0 +1,39 @@
+""" wrapid: Web Resource Application Programming Interface built on Python WSGI.
+
+Login to an account other than 'anonymous'. Uses Basic Authentication.
+"""
+
+import logging
+
+from .resource import *
+from .utils import get_account_basic_authentication
+
+
+class GET_Login(Method):
+    "Login to an account other than 'anonymous'. Uses Basic Authentication."
+
+    def __init__(self, realm, get_account):
+        "Set the realm and account retrieval and authentication function."
+        self.realm = realm
+        self.get_account = get_account
+
+    def __call__(self, resource, request, application):
+        account = get_account_basic_authentication(request,
+                                                   self.realm,
+                                                   self.get_account)
+        if account['name'] == 'anonymous':
+            logging.debug('wrapid: anonymous login is not good enough')
+            raise HTTP_UNAUTHORIZED_BASIC_CHALLENGE(realm=self.realm)
+        else:
+            try:
+                url = request.get_value('url')
+                if not url: raise KeyError
+            except KeyError:
+                url = application.url
+            # The cookie remedies an apparent deficiency of several
+            # human browsers: For some pages in the site (notably
+            # the application root '/'), the authentication data does not
+            # seem to be sent voluntarily by the browser.
+            cookie = "%s-login=yes; Path=%s" % (self.realm, application.path)
+            logging.debug("wrapid: login redirect to %s", url)
+            raise HTTP_SEE_OTHER(Location=url, Set_Cookie=cookie)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 """ wrapid: Web Resource Application Programming Interface built on Python WSGI.
 
-Various general utility functions.
+Various utility functions.
 """
 
 import base64
@@ -28,3 +28,52 @@ def basic_authentication(request, realm, require=True):
         else:
             raise
     return base64.standard_b64decode(parts[1]).split(":", 1)
+
+def identify_account(variables, get_account):
+    """Return the account dictionary given the 'variables' lookup,
+    which is assumed to contain an item 'account'.
+    The function 'get_account' takes an account name and returns
+    the account dictionary **without any authentication**.
+    It must raise KeyError if there is no such account.
+    This handles the case where an account name containing a dot and
+    a short (<=4 chars) last name, which will otherwise be confused
+    for a FORMAT specification.
+    Raise KeyError if no account found.
+    """
+    try:
+        result = get_account(variables['account'])
+    except KeyError:
+        if variables.get('FORMAT'):
+            name = variables['account'] + variables['FORMAT']
+            try:
+                result = get_account(name)
+            except KeyError:
+                raise
+            else:
+                variables['account'] += variables['FORMAT']
+                variables['FORMAT'] = None
+        else:
+            raise
+    return result
+
+def get_account_basic_authentication(request, realm, get_account):
+    """Return the account dictionary given basic authentication data
+    in the request. If none (and none available according to the cookie),
+    then return the account 'anonymous'.
+    Raise KeyError if no account found.
+    """
+    try:
+        name, password = basic_authentication(request, realm, require=False)
+    except ValueError:              # No authentication data
+        # The cookie remedies an apparent deficiency of several
+        # human browsers: For some pages in the site (notably
+        # the application root '/'), the authentication data does not
+        # seem to be sent voluntarily by the browser.
+        if request.cookie.has_key("%s-login" % realm):
+            raise HTTP_UNAUTHORIZED_BASIC_CHALLENGE(realm=realm)
+        return get_account('anonymous')
+    else:
+        try:
+            return get_account(name, password)
+        except (KeyError, ValueError):
+            return get_account('anonymous')
